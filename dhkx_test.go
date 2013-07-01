0c763f0ff37752f3a48aca7abb363fb9e255388b
Nicholas Helke <nhelke@gmail.com>
2013-07-01 14:46:04 -0700

    Added a CreateGroup method to dhgroup.go
    Also added an associated test to dhkx_test.go and fleshed out the documentation
    in dhgroup.go
    
    Fixes monnand#1

--- dhkx_test.go
+++ dhkx_test.go
@@ -19,13 +19,14 @@ package dhkx
 
 import (
 	"fmt"
+	"math/big"
 	"testing"
 )
 
 type peer struct {
-	priv *DHKey
+	priv  *DHKey
 	group *DHGroup
-	pub *DHKey
+	pub   *DHKey
 }
 
 func newPeer(g *DHGroup) *peer {
@@ -88,3 +89,15 @@ func TestKeyExchange(t *testing.T) {
 	}
 }
 
+func TestCustomGroupKeyExchange(t *testing.T) {
+	p, _ := new(big.Int).SetString("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A63A36210000000000090563", 16)
+	g := new(big.Int).SetInt64(2)
+	group := CreateGroup(p, g)
+	p1 := newPeer(group)
+	p2 := newPeer(group)
+
+	err := exchangeKey(p1, p2)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+}
